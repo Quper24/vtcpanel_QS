0627e862577c508da61ba0392f26867dfc4765a7
Leskin Maksim <36654996+Quper24@users.noreply.github.com>
2025-12-22 06:29:32 +0300

    update data

--- script.js
+++ script.js
@@ -1,87 +1,186 @@
 let rawData;
+let currentDate;
+let compareDate;
 let currentData = [];
 let sortKey = null;
 let sortDir = 1;
+let sortDiff = false;
 
 fetch("data.json")
-  .then((res) => res.json())
+  .then((r) => r.json())
   .then((data) => {
     rawData = data.employees;
     initDates();
   });
 
 function initDates() {
-  const select = document.getElementById("dateSelect");
-  Object.keys(rawData)
-    .sort(
-      (a, b) =>
-        new Date(a.split(".").reverse().join("-")) -
-        new Date(b.split(".").reverse().join("-")),
-    )
-    .forEach((date) => {
-      const option = document.createElement("option");
-      option.value = date;
-      option.textContent = date;
-      select.appendChild(option);
-    });
+  const main = document.getElementById("dateMain");
+  const cmp = document.getElementById("dateCompare");
 
-  select.addEventListener("change", () => {
-    loadDate(select.value);
+  const dates = Object.keys(rawData).sort(
+    (a, b) =>
+      new Date(a.split(".").reverse().join("-")) -
+      new Date(b.split(".").reverse().join("-")),
+  );
+
+  dates.forEach((d) => {
+    main.add(new Option(d, d));
+    cmp.add(new Option(d, d));
   });
 
-  loadDate(select.value);
+  main.value = dates[dates.length - 2] || dates[dates.length - 1];
+  cmp.value = dates[dates.length - 1];
+
+  currentDate = main.value;
+  compareDate = cmp.value;
+
+  main.onchange = () => {
+    currentDate = main.value;
+    updateData();
+  };
+
+  cmp.onchange = () => {
+    compareDate = cmp.value;
+    updateData();
+  };
+
+  updateData();
 }
 
-function loadDate(date) {
-  currentData = [...rawData[date]];
-  renderTable();
+function getDiff(id, field) {
+  const cur = rawData[currentDate]?.find((u) => u.id_user === id);
+  const cmp = rawData[compareDate]?.find((u) => u.id_user === id);
+
+  // новый водитель
+  if (cmp && !cur) {
+    return {
+      type: "new",
+      value: cmp[field] ?? 0,
+      diff: cmp[field] ?? 0,
+    };
+  }
+
+  // водитель был, но ушёл (обычно не рендерится)
+  if (cur && !cmp) {
+    return {
+      type: "left",
+      value: null,
+      diff: -(cur[field] ?? 0),
+    };
+  }
+
+  // есть в обеих датах
+  const a = Number(cur?.[field] ?? 0);
+  const b = Number(cmp?.[field] ?? 0);
+
+  return {
+    type: "normal",
+    value: b,
+    diff: a - b,
+  };
+}
+
+function valueWithDiff(data) {
+  if (data.type === "new") {
+    return `
+      ${data.value}
+      <span class="diff plus">NEW</span>
+    `;
+  }
+
+  if (data.type === "left") {
+    return `<span class="diff minus">LEFT</span>`;
+  }
+
+  if (data.diff === 0) {
+    return `${data.value} <span class="diff zero">(0)</span>`;
+  }
+
+  const cls = data.diff < 0 ? "plus" : "minus";
+  const sign = data.diff < 0 ? "+" : "";
+
+  return `
+    ${data.value}
+    <span class="diff ${cls}">
+      (${sign}${data.diff.toFixed(2)})
+    </span>
+  `;
+}
+
+function updateData() {
+  currentData = [...rawData[currentDate]];
+
+  // если сортировка уже выбрана — применяем её снова
+  if (sortKey) {
+    sortBy(sortKey, sortDiff);
+  } else {
+    renderTable();
+  }
 }
 
 function renderTable() {
   const tbody = document.querySelector("tbody");
   tbody.innerHTML = "";
 
-  currentData.forEach((item) => {
+  currentData.forEach((u) => {
     const tr = document.createElement("tr");
 
     tr.innerHTML = `
-      <td><img class="avatar" src="${item.image_url}" alt=""></td>
-      <td><a href="https://vtcpanel.com/id${item.id_user}" target="_blank">${
-      item.steam_name
+  <td><img class="avatar" src="${u.image_url}"></td>
+  <td><a href="https://vtcpanel.com/id${u.id_user}" target="_blank">${
+      u.steam_name
     }</a></td>
-      <td>${item.name}</td>
-      <td>${item.family}</td>
-      <td>${item.country}</td>
-      <td>${item.city}</td>
-      <td class="number">${Number(item.pokazatel)}</td>
-      <td class="number">${item.year}</td>
-      <td class="number">${item.karma}</td>
-      <td class="number">${item.karma_vtc}</td>
-      <td class="number">${item.point_m}</td>
-      <td class="number">${item.point}</td>
-    `;
+  <td>${u.name}</td>
+  <td>${u.family}</td>
+  <td>${u.country}</td>
+  <td>${u.city}</td>
+  
+  
+  <td class="number">${u.pokazatel}</td>
+  <td class="number">
+    ${valueWithDiff(getDiff(u.id_user, "pokazatel"))}
+  </td>
+  
+  
+  <td class="number">${u.karma}</td>
+  <td class="number">
+    ${valueWithDiff(getDiff(u.id_user, "karma"))}
+  </td>
+  
+  <td class="number">${u.karma_vtc}</td>
+  <td class="number">
+    ${valueWithDiff(getDiff(u.id_user, "karma_vtc"))}
+  </td>
+  
+  <td class="number">${u.point_m}</td>
+  <td class="number">
+    ${valueWithDiff(getDiff(u.id_user, "point_m"))}
+  </td>
+
+  
+  <td class="number">${u.point}</td>
+  <td class="number">
+    ${valueWithDiff(getDiff(u.id_user, "point"))}
+  </td>
+
+  `;
 
     tbody.appendChild(tr);
   });
 }
 
-function sortBy(key) {
-  if (sortKey === key) {
-    sortDir *= -1;
-  } else {
+function sortBy(key, isDiff) {
+  if (sortKey === key && sortDiff === isDiff) sortDir *= -1;
+  else {
     sortKey = key;
+    sortDiff = isDiff;
     sortDir = 1;
   }
 
   currentData.sort((a, b) => {
-    let valA = a[key];
-    let valB = b[key];
-
-    if (!isNaN(valA) && !isNaN(valB)) {
-      return (Number(valA) - Number(valB)) * sortDir;
-    }
-
-    return String(valA).localeCompare(String(valB), "ru") * sortDir;
+    const A = isDiff ? getDiff(a.id_user, key).diff : a[key];
+    const B = isDiff ? getDiff(b.id_user, key).diff : b[key];
+    return (Number(A) - Number(B)) * sortDir;
   });
 
   renderTable();
@@ -89,7 +188,6 @@ function sortBy(key) {
 
 document.querySelectorAll("th").forEach((th) => {
   const key = th.dataset.key;
-  if (!key || key === "image_url") return;
-
-  th.addEventListener("click", () => sortBy(key));
+  if (!key) return;
+  th.onclick = () => sortBy(key, th.dataset.diff === "true");
 });
